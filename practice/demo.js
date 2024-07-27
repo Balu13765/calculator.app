debf2bab11986096965f646d58079e13cc06d0d4
Balu13765 <balajiganugapichi@gmail.com>
2024-07-28 03:44:37 +0530

    committing

---
+++ practice/demo.js
@@ -0,0 +1,106 @@
+
+/*a=10;
+b=23;
+console.log(a);
+console.log(b+a);*/
+
+//2//
+
+ /*var a=10;
+ var b='java script';
+ console.log(a);
+ console.log(b);
+ console.log(a+b);
+ var c=true;
+ console.log(a+b+c);*/
+
+ //3//
+
+ /*var car="tata";
+ let price=3000;
+ price++;
+ var car="suzuki";
+ console.log(car);
+ console.log(price);*/
+
+ //4//
+//   typeof()   //
+ /*var a=10;
+ var b='9';
+ console.log(typeof(a));
+ console.log(typeof(b));
+ console.log(a+b);*/
+
+ //5//
+
+ /*var a ="hello World";
+ console.log(a);
+ //length//
+ console.log(a.length);
+// indexOf//
+console.log(a.indexOf("l"));
+//charAt//
+console.log(a.charAt(9));
+//toUppercase//
+console.log(a.toUpperCase());
+//toLowerCase//
+console.log(a.toLowerCase());*/
+
+//6//
+
+//array//
+
+/*let a=["balu","shannu","nihan"]
+console.log(   "name :"+ a[1]);*/
+
+//7//
+
+//conditional Statements //
+/*let a=10,b= 20, c=30;
+
+
+if(a>b)
+    {
+        console.log("a is big");
+
+    }
+    else if(c>b)       {
+
+     console.log("c is big")
+    }
+    else{
+        console.log("b is big")
+
+    }*/
+
+        //8//
+
+        //     loops     //
+
+//for loop//
+/*for(a=1;a<=50;a++){
+    console.log("page  no is "+a);
+    if(a==5){
+        console.log("break sta6tement occured ");
+        break;
+    }
+}*/
+
+//9//
+//function//
+
+/*function first(a){
+    console.log(" this is a function"+ a)
+}
+first(call);*/
+if (false)
+
+
+
+ 
+
+
+
+
+
+
